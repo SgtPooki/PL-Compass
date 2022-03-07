905b81da521bf02d26dc784eac00832dda4864a7
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-03-07 14:35:47 -0800

    add eslint and prettier files

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+//   files: ['./src/**/*.ts{,x}'],
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    semi: ["error", "never"]
+  },
+}
