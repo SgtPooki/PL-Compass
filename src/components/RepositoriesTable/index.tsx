e2b429ae03d8530528ea553c5e398c93e89ff873
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-03-10 09:39:28 -0800

    Add react-table filtering
    The code added was taken almost verbatim from https://codesandbox.io/s/github/tannerlinsley/react-table/tree/v7/examples/filtering

--- src/components/RepositoriesTable/index.tsx
+++ src/components/RepositoriesTable/index.tsx
@@ -1,12 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useMemo } from 'react'
 import { useAsync } from 'react-async'
-import { Column, useTable, useSortBy } from 'react-table'
+import {
+  Column,
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+} from 'react-table'
 
 import { fetchRepos } from './fetchRepos'
 import { RepositoryRow } from './RepositoryRow'
 import { ErrorBoundary } from '../ErrorBoundary'
 import { LoadingRow } from './LoadingRow'
+import { matchSorter } from 'match-sorter'
 
 const RepositoriesTable = () => {
   const columns: Column<EcosystemResearch.Repository>[] = useMemo(
@@ -22,6 +30,8 @@ const RepositoriesTable = () => {
       {
         Header: 'Organization',
         accessor: 'org',
+        Filter: SelectColumnFilter,
+        filter: 'includes',
       },
       {
         Header: 'Ecosystem Score',
@@ -58,7 +68,63 @@ const RepositoriesTable = () => {
 
   return <Table columns={columns} data={data} />
 }
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+any) {
+  const count = preFilteredRows.length
 
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+// This is a custom filter UI for selecting
+// a unique option from a list
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}: any) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach((row: any) => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option: any, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  )
+}
+function fuzzyTextFilterFn(rows: any, id: any, filterValue: any) {
+  return matchSorter(rows, filterValue, {
+    keys: [(row: any) => row.values[id]],
+  })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = (val: any) => !val
 const Table = ({
   columns,
   data,
@@ -66,14 +132,53 @@ const Table = ({
   columns: readonly Column<EcosystemResearch.Repository>[]
   data: readonly EcosystemResearch.Repository[]
 }) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
+  const filterTypes = useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows: any, id: any, filterValue: any) => {
+        return rows.filter((row: any) => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
       },
-      useSortBy
-    )
+    }),
+    []
+  )
+  const defaultColumn = useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, // Be sure to pass the defaultColumn option
+      filterTypes,
+    },
+    useFilters, // useFilters!
+    useGlobalFilter, // useGlobalFilter!
+    useSortBy
+  )
 
   return (
     <ErrorBoundary>
@@ -106,6 +211,10 @@ const Table = ({
                               : ' 🔼'
                             : ''}
                         </span>
+                        {/* Render the columns filter UI */}
+                        <div>
+                          {column.canFilter ? column.render('Filter') : null}
+                        </div>
                       </th>
                     )
                   })}
